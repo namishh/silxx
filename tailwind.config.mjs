4568a9fdae8120a0d1cc2864ed695486370cbec7
chadcat7 <namishpande2007@gmail.com>
2023-10-25 01:44:41 +0530

    template ready

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    config: {
+    variants: {
+      extend: {
+        translate: ['group-hover', 'hover'],
+      },
+    },
+    },
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
